b6fcb7a10830d5f164f4e95ca0238516446b8a88
mars <hroachewilson@gmail.com>
2019-03-20 09:55:54 +1000

    fixed AEV Interface button and made aev/control_node use a custom signal handler to detect shutdown and publish control_mode=false

--- ros/src/actuation/vehicles/packages/aev/nodes/control_node/control_node.cpp
+++ ros/src/actuation/vehicles/packages/aev/nodes/control_node/control_node.cpp
@@ -1,10 +1,42 @@
 #include <ros/ros.h>
 #include <std_msgs/Bool.h>
+#include <signal.h>
+#include <ros/ros.h>
+#include <ros/xmlrpc_manager.h>
 
+// Signal-safe flag for whether shutdown is requested
+sig_atomic_t volatile g_request_shutdown = 0;
+
+// Replacement SIGINT handler
+void mySigIntHandler(int sig)
+{
+  g_request_shutdown = 1;
+}
+
+// Replacement "shutdown" XMLRPC callback
+void shutdownCallback(XmlRpc::XmlRpcValue& params, XmlRpc::XmlRpcValue& result)
+{
+  int num_params = 0;
+  if (params.getType() == XmlRpc::XmlRpcValue::TypeArray)
+    num_params = params.size();
+  if (num_params > 1)
+  {
+    std::string reason = params[1];
+    ROS_WARN("Shutdown request received. Reason: [%s]", reason.c_str());
+    g_request_shutdown = 1; // Set flag
+  }
+
+  result = ros::xmlrpc::responseInt(1, "", 0);
+}
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "aev/control_node");
+  ros::init(argc, argv, "aev/control_node", ros::init_options::NoSigintHandler);
+	signal(SIGINT, mySigIntHandler);
+
+	// Override XMLRPC shutdown
+  ros::XMLRPCManager::instance()->unbind("shutdown");
+  ros::XMLRPCManager::instance()->bind("shutdown", shutdownCallback);
 
 	ros::NodeHandle nh_;
 	std_msgs::Bool msg;
@@ -12,16 +44,23 @@ int main(int argc, char **argv)
 
 	msg.data = true;
 
-  // Control node publisher
+  // Instantiate control node publisher and publish control_mode=true
   control_mode_pub    = nh_.advertise<std_msgs::Bool>("/aev/control_mode", 10, true);
 	control_mode_pub.publish(msg);
 
-	// Wait until node is shutdown before sending false
-	while(ros::ok());
+	// Do our own spin loop
+  while (!g_request_shutdown)
+  {
+    // Do non-callback stuff
+
+    ros::spinOnce();
+    usleep(100000);
+  }
 
 	// Node is shutting down. Unlatch autopilot
-	msg.data = false;
+	msg.data = false;	
 	control_mode_pub.publish(msg);
+	ros::shutdown();
 
   return 0;
 }
