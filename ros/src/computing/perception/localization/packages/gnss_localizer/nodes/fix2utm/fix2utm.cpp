e0bc2f29d2f4bccae64c596140707bed75a7603b
hroachewilson <hroachewilson@gmail.com>
2019-05-23 20:30:26 +1000

    added 006 launch files and fixed fix2utm

--- ros/src/computing/perception/localization/packages/gnss_localizer/nodes/fix2utm/fix2utm.cpp
+++ ros/src/computing/perception/localization/packages/gnss_localizer/nodes/fix2utm/fix2utm.cpp
@@ -21,11 +21,13 @@
 #include <sensor_msgs/NavSatFix.h>
 #include <std_msgs/Bool.h>
 #include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
 #include <gps_common/conversions.h>
 #include <string>
 
 #include <iostream>
 #include <gnss/geo_pos_conv.hpp>
+#include <cmath>
 
 static ros::Publisher pose_publisher;
 
@@ -35,8 +37,12 @@ static std_msgs::Bool gnss_stat_msg;
 static geometry_msgs::PoseStamped _prev_pose;
 static geometry_msgs::Quaternion _quat;
 static double yaw;
-
 static std::string _fix;
+static double _origin_easting;
+static double _origin_northing;
+static double _map_offset_x;
+static double _map_offset_y;
+static double rotation_offset;
 
 static void GNSSCallback(const sensor_msgs::NavSatFixConstPtr &msg)
 {
@@ -49,23 +55,26 @@ static void GNSSCallback(const sensor_msgs::NavSatFixConstPtr &msg)
     return;
   }
 
-  double northing, easting;
+  double northing, easting, x_rot, y_rot;
   std::string zone;
 
   gps_common::LLtoUTM(msg->latitude, msg->longitude, northing, easting, zone);
+  x_rot = cos(rotation_offset)*(easting - _origin_easting) - sin(rotation_offset)*(northing - _origin_northing);
+  y_rot = sin(rotation_offset)*(easting - _origin_easting) + cos(rotation_offset)*(northing - _origin_northing);
 
   static tf::TransformBroadcaster pose_broadcaster;
   tf::Transform pose_transform;
   tf::Quaternion pose_q;
-
+  
   geometry_msgs::PoseStamped pose;
   pose.header = msg->header;
   // pose.header.stamp = ros::Time::now();
   pose.header.frame_id = "map";
-  pose.pose.position.x = easting;
-  pose.pose.position.y = northing;
+  pose.pose.position.x = x_rot + _map_offset_x;
+  pose.pose.position.y = y_rot + _map_offset_y;
   pose.pose.position.z = msg->altitude;
 
+ 
   // set gnss_stat
   if (pose.pose.position.x == 0.0 || pose.pose.position.y == 0.0 || pose.pose.position.z == 0.0)
   {
@@ -78,7 +87,7 @@ static void GNSSCallback(const sensor_msgs::NavSatFixConstPtr &msg)
 
   double distance = sqrt(pow(pose.pose.position.y - _prev_pose.pose.position.y, 2) +
                          pow(pose.pose.position.x - _prev_pose.pose.position.x, 2));
-  std::cout << "distance : " << distance << std::endl;
+  //std::cout << "distance : " << distance << std::endl;
 
   if (distance > 0.2)
   {
@@ -107,6 +116,11 @@ int main(int argc, char **argv)
   ros::NodeHandle nh;
   ros::NodeHandle private_nh("~");
   private_nh.getParam("fix", _fix);
+  private_nh.getParam("origin_easting", _origin_easting);
+  private_nh.getParam("origin_northing", _origin_northing);
+  private_nh.getParam("map_offset_x", _map_offset_x);
+  private_nh.getParam("map_offset_y", _map_offset_y);
+  private_nh.getParam("rotation_offset", rotation_offset);
   pose_publisher = nh.advertise<geometry_msgs::PoseStamped>("gnss_pose", 1000);
   stat_publisher = nh.advertise<std_msgs::Bool>("/gnss_stat", 1000);
   ros::Subscriber gnss_pose_subscriber = nh.subscribe(_fix, 100, GNSSCallback);
